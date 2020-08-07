ed04d6405b5db1841dff7c7d18d627c7995c722b
CodeCommit-at-966693457861 <riyazulhaque922@gmail.com>
2020-08-07 10:27:54 -0400

    HEy this is the change in temp Branch

--- ca2020.py
+++ ca2020.py
@@ -4,5 +4,7 @@ z=30
 t=1000
 print ("I am adding a line of code at the top")
 print ("Just a testing to show you the pull command")
+print ("This is the temp line to check the working of testing branch")
 res=x+y+z+t
 print ("hello my sum is",res)
+print ("WOW that worked")
